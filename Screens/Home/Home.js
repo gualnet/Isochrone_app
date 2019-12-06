622e7d877f3bc6d094db4eaf61357d300a29948f
galy <galy@student.42.fr>
2019-12-06 09:43:17 +0100

    Contact access request moved to home screen

--- Screens/Home/Home.js
+++ Screens/Home/Home.js
@@ -17,15 +17,22 @@ class HomeScreen extends React.Component {
   askForPermissions = async () => {
     // ask the user for the permissions
     await MyPermissions.Location();
-
     // get the permissions status in the config object
     if (!config.Permissions.Location) {
       console.error('[ERROR] Location not authorized');
       return;
     };
-
+    // Get the location status
     let location = await Location.getCurrentPositionAsync({});
     console.log('location', location);
+
+    // ask the user for the permissions
+    await MyPermissions.Contact();
+    // get the permissions status in the config object
+    if (!config.Permissions.Contacts) {
+      console.error('[ERROR] Contact access not authorized');
+      return;
+    };
 
   };
 
@@ -34,9 +41,6 @@ class HomeScreen extends React.Component {
   }
 
   render() {
-
-    
-
     return (
       <View style={styles.container}>
         <Text>FIND YOUR BAR</Text>
