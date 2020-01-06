39c02980443fac1144cf830ae4684d4352495c37
galy <galy@student.42.fr>
2020-01-06 10:22:58 +0100

    update event item

--- Components/EventItem/EventItem.jsx
+++ Components/EventItem/EventItem.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { TouchableOpacity, View, Text } from 'react-native';
-import styles from './style';
+import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
 
-// import style from './style';
+import { theme } from '../../libs'
 
 class EventItem extends React.Component {
   constructor(props) {
@@ -17,10 +16,10 @@ class EventItem extends React.Component {
   render() {
     // console.log('RENDER EVENT ITEM', this.props.Event)
     return (
-      <View style={styles.eventItem} >
-        <TouchableOpacity
+      <View style={styles.viewShadow} >
+        <TouchableOpacity style={styles.eventItem}
           onPress={() => this.navigateToEventDetails()} >
-          <Text>EVENT ITEM</Text>
+          {/* <Text>EVENT ITEM</Text> */}
           <Text>ID {this.props.Event.id}</Text>
           <Text>Event: {this.props.Event.name}</Text>
           <Text>Date: {this.props.Event.date}</Text>
@@ -31,3 +30,37 @@ class EventItem extends React.Component {
 };
 
 export default EventItem;
+
+const styles = StyleSheet.create({
+  eventItem: {
+    marginTop: 5,
+    marginBottom: 5,
+    marginLeft: 5,
+    marginRight: 5,
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 20,
+    paddingRight: 20,
+
+    // Shadow effect for ios
+    backgroundColor: theme.colors.mainBackground,
+    borderRadius: theme.sizes.inputBorderRadius,
+    borderWidth: theme.sizes.inputBorderWidth,
+    shadowColor: theme.colors.shadowColorTop,
+    shadowOffset: { width: 8, height: 8 },
+    shadowRadius: 8,
+    shadowOpacity: 0.2,
+    // shadow effect for android
+    elevation: 2,
+  },
+  viewShadow: {
+    // special shadow effect for ios
+    borderRadius: theme.sizes.inputBorderRadius,
+    shadowColor: theme.colors.shadowColorDown,
+    shadowOffset: { width: -8, height: -8 },
+    shadowRadius: 8,
+    shadowOpacity: 1,
+    marginTop: 5,
+    marginBottom: 5,
+  },
+});
