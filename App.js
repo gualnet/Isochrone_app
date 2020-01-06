ebf0fc7c61c7112b8aada1822f2fc5783cb248c3
galy <galy@student.42.fr>
2020-01-06 10:52:33 +0100

    start integration ui-kit ative-base

--- App.js
+++ App.js
@@ -1,16 +1,44 @@
 import React from 'react';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
 import { Provider } from 'react-redux';
-import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import Navigation from './Navigation/navigation';
+import { StyleSheet, Text, View } from 'react-native';
+
 import Store from './Store/configStore';
 
-export default function App() {
-  return (
-    <Provider store={Store}>
-      <Navigation></Navigation>
-    </Provider>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+    this.setState({ isReady: true });
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
+
+    return (
+      <Provider store={Store}>
+        <Navigation></Navigation>
+      </Provider>
+    );
+  }
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
